6d795a2aeaecd57346b0c9c6e535903de34f761d
Tito <tito.parra4@@hotmail.com>
2019-10-14 23:13:46 +0200

    Class OrderDetail

---
+++ TheStore.BL/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheStore.BL
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal PurchasePrice { get; set; }
+    }
+}
