092c6e67f6f70ae991c978b97be7f98a1a3fcf3d
Tito <tito.parra4@@hotmail.com>
2019-10-14 23:11:38 +0200

    Class Order

---
+++ TheStore.BL/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheStore.BL
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+    }
+}
