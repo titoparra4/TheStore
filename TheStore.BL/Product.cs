39c36b8aa2d2829d4e49779410c564196dc423a1
Tito <tito.parra4@@hotmail.com>
2019-10-14 23:09:51 +0200

    Class Product

---
+++ TheStore.BL/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheStore.BL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal price { get; set; }
+    }
+}
